0fbfa36a21354205b91bdbe5da0c329b9df22bb2
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2020-10-16 13:24:49 +0200

    Add basic graphql functionality to test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 import { __prod__ } from "./constant";
-import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   
@@ -14,6 +16,19 @@ const main = async () => {
   // Create instance of post that is ready to be inserted into the database 
   // const post = orm.em.create(Post, new Post("My First Post"));
   // await orm.em.persistAndFlush(post);
+
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    })
+  });
+
+  apolloServer.applyMiddleware({app});
+  
+  app.listen(3000, () => {
+    console.log('Server started on localhost:3000');
+  });
 };
 
 main().catch(err => {
