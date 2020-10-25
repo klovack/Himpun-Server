c6de49b98b4fd47915bb3b809c1bf6ca7f9c656c
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2020-10-25 14:39:57 +0100

    Update all resolvers to use typeorm

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,64 +1,64 @@
-import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
+import { validate } from "class-validator";
+import { Arg, Int, Mutation, Query, Resolver } from "type-graphql";
 
 import { Post } from "../entities/Post";
-import { HimpunContext } from "../types";
 
 @Resolver(Post)
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() ctx: HimpunContext): Promise<Post[]> {
-    
-    return ctx.em.find(Post, {});
+  posts(): Promise<Post[]> {
+    return Post.find();
   }
 
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg('id', () => Int) id: number,
-    @Ctx() ctx: HimpunContext): Promise<Post | null> {
-    
-    return ctx.em.findOne(Post, { id });
-  }
-
-  @Mutation(() => Post)
-  async createPost(
-    @Arg('title', () => String) title: string,
-    @Ctx() ctx: HimpunContext): Promise<Post> {
-
-    const post = ctx.em.create(Post, {title});
-    await ctx.em.persistAndFlush(post);
-    
-    return post;
+  post(@Arg('id', () => String) id: string): Promise<Post | null | undefined> {
+    return Post.findOne(id);
   }
 
   @Mutation(() => Post, {nullable: true})
+  async createPost(@Arg('title', () => String) title: string): Promise<Post|null> {
+    const post = new Post({
+      title,
+    });
+    const errors = await validate(post);
+    if (errors.length > 0) {
+      return null;
+    }
+
+    return post.save();
+  }
+
+  @Mutation(() => Post, { nullable: true })
   async updatePost(
-    @Arg('id', () => Int) id: number,
-    @Arg('title', () => String, { nullable: true }) title: string,
-    @Ctx() ctx: HimpunContext): Promise<Post | null> {
-    
-    const post = await ctx.em.findOne(Post, {id});
+    @Arg('id', () => String) id: string,
+    @Arg('title', () => String, { nullable: true }) title: string
+    ): Promise<Post | null> {
+
+    const post = await Post.findOne(id);
     if (!post) {
       return null;
     }
 
     if (typeof title !== "undefined") {
-      post.title = title;
-      await ctx.em.persistAndFlush(post);
+      await Post.update({ id }, {title});
     }
-    
+
     return post;
   }
 
   @Mutation(() => Boolean)
   async deletePost(
-    @Arg('id', () => Int) id: number,
-    @Ctx() ctx: HimpunContext)
-    : Promise<boolean> {
-    try {
-      await ctx.em.nativeDelete(Post, {id});
+    @Arg('id', () => Int) id: number
+    ): Promise<boolean> {
+      const del = await Post.delete(id);
+      if (del.affected! > 0) {
+        return true;
+      }
+
+      if(del.affected! <= 0) {
+        return false;
+      }
+
       return true;
-    } catch {
-      return false;
-    }
   }
 }
